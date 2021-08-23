3bf5cf23cc44a82965040d23a601e82652918698
Nikola Bulat <nbulat99@gmail.com>
2021-08-23 10:25:41 +0200

    Removed unused import

--- src/test/java/com/prime/rush_hour/services/UserServiceLayerTest.java
+++ src/test/java/com/prime/rush_hour/services/UserServiceLayerTest.java
@@ -30,7 +30,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
